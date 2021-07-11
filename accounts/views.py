f464e8938f6c0ac1a65f7933f9b28c87b100342e
Mohammed <elkhamlichimedn1et@gmail.com>
2021-07-11 16:59:37 +0000

    Update

--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 from accounts.models import Student
 from django.http import HttpResponseRedirect
-
+from accounts.forms import StudentForm
 # Create your views here.
+
+# Students Details.
 def base(request):
     students = Student.objects.all()
+    form = StudentForm()
     if request.method == "POST":
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
+        form = StudentForm(request.POST)
         if first_name and last_name and email:
-            Student.objects.create(first_name=first_name, last_name=last_name, email=email)
-            print(first_name, last_name, email)
+            form.first_name= first_name
+            form.last_name = last_name
+            form.email = email
+            form.save(True)
+            return HttpResponseRedirect('http://127.0.0.1:8000/')
         else:
+            form = StudentForm()
             return HttpResponseRedirect('.')
     context = {
-        "dev": 'Mohammed'
+        "dev": 'Mohammed',
+        'form': form
     }
     return render(request, 'base.html', context)
